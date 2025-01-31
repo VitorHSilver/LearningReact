331d2aee6a4cddc0344d98ace0e775f08ca8271a
VitorHSilver <vitor-henrique1996@hotmail.com>
2025-01-31 17:26:36 -0300

    useRef, setTimeOut, Comments

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,37 @@
 import React from 'react';
 import './App.css';
 
-import Home from './components/Home';
-
 const App = () => {
-     return <Home />;
+     const [comments, setComments] = React.useState([]);
+     const [input, setInput] = React.useState('');
+     const inputElement = React.useRef();
+     const handleClick = () => {
+          setComments([...comments, input]);
+          setInput('');
+          inputElement.current.focus();
+     };
+     return (
+          <div className="m-4">
+               <h1 className="text-lg font-extrabold mb-4">comments:</h1>
+               <ul>
+                    {comments.map((comment, index) => (
+                         <li key={index}>{comment}</li>
+                    ))}
+               </ul>
+               <div className="mt-4">
+                    <input
+                         type="text"
+                         className="border"
+                         value={input}
+                         ref={inputElement}
+                         onChange={({ target }) => setInput(target.value)}
+                    />
+                    <button className="bg-blue-300 p-2 rounded-md ml-2" onClick={handleClick}>
+                         Enviar
+                    </button>
+               </div>
+          </div>
+     );
 };
 
 export default App;
