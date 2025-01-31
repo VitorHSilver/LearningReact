bf7cfd51e28fbdc42f66e5c42f64376954bdbf8a
VitorHSilver <vitor-henrique1996@hotmail.com>
2025-01-31 12:15:17 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,35 @@
 import React from 'react';
 import './App.css';
 
-import Home from './components/Home';
-
 const App = () => {
-     return <Home />;
+     const [contar, setContar] = React.useState(0);
+     const [dados, setDados] = React.useState(null);
+
+     React.useEffect(() => {
+          fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+               .then((response) => response.json())
+               .then((json => setDados(json)));
+     }, []);
+     React.useEffect(() => {
+          console.log('Inicializou');
+     }, []);
+
+     React.useEffect(() => {
+          document.title = 'Total ' + contar;
+     }, [contar]);
+     return (
+          <div>
+               {dados && (
+                    <div>
+                         <h1>{dados.nome}</h1>
+                         <p>R$ {dados.preco * contar}</p>
+                    </div>
+               )}
+               <button className="bg-emerald-100  p-2 m-4" onClick={() => setContar(contar + 1)}>
+                    Click: {contar}
+               </button>
+          </div>
+     );
 };
 
 export default App;
